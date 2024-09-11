f8ed72706666779c381c104f0144a77a356677b0
Yuzhou LAN <lanyuzhou22@mails.ucas.ac.cn>
2024-09-11 23:49:18 +0800

    add .gitignore

---
+++ src/hw1/Red_envelop.java
@@ -0,0 +1,35 @@
+package hw1;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Red_envelop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("输入红包总金额: ");
+        double money = input.nextDouble();  // 红包总金额
+        System.out.print("输入红包总数量: ");
+        int num = input.nextInt();  // 红包总数量
+
+        // 计算每个红包的金额
+        int[] res = ;
+
+//        if (celsius < -273.15){
+//            // Input error
+//            System.out.println("输入值错误（不能低于绝对零度-237.15），程序已退出");
+//            System.exit(1);
+//        } else if (celsius > 1.416833e32) {
+//            // Input error
+//            System.out.println("输入值错误（不能高于普朗克温度1.416833(85)x10^32K），程序已退出");
+//            System.exit(1);
+//        } else {
+//            // Convert Celsius to Fahrenheit
+//            double fahrenheit = (9.0 / 5) * celsius + 32;
+//            System.out.println("摄氏 " + celsius + " 度等于华氏 " +
+//                    fahrenheit + " 度");
+//        }
+
+        input.close();
+    }
+}
